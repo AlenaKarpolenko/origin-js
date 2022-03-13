47d0e60395be12a168c3e78efe5edc9b49bd7c70
AlenaKarpolenko <alenac190590@mail.ru>
2022-03-13 20:23:46 +0700

    added 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
   if (a === 0) {
@@ -19,6 +21,31 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
+  let percentTest = Number(percent);
+  let contributionTest = Number(contribution);
+  let amountTest = Number(amount);
+  
+  if (percentTest != Number(percent)){
+	totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+    return totalAmount	
+  } else if (contributionTest != Number(contribution)){
+	totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+    return totalAmount  
+  } else if (amountTest != Number(amount)){
+	totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+    return totalAmount  
+	}
+	  else {
+  let credit = amount - contribution;
+  let dateMonth = date.getMonth();
+  let dateYear = date.getFullYear();
+  let dateNow = new Date(Date.now());
+  let dateNowMonth = dateNow.getMonth();
+  let dateNowYear = dateNow.getFullYear();
+  let month = (dateYear-dateNowYear)*12+(dateMonth-dateNowMonth);
+  
+  let payment = credit * (percent/12/100 + (percent/12/100 / (((1 + percent/12/100)**month - 1))));
+  totalAmount = payment * month;
 
-  return totalAmount;
-}
+return Number(totalAmount.toFixed(2));}}
+
